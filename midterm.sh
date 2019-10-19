e741186c3d86adf17c1a32e95346940569838340
EC2 Default User <ec2-user@ip-172-31-81-151.ec2.internal>
2019-10-19 02:06:18 +0000

    midterm – backup txt

--- midterm.sh
+++ midterm.sh
@@ -5,7 +5,7 @@
 
 ACTION=${1}
 
-VERSION=1.0
+VERSION=2.0
 
 function system_update() {
 			sudo yum update -y
@@ -26,6 +26,21 @@ echo "$VERSION"
 }
 
 
+function show_config() {
+	touch awscli.txt
+	aws ec2 describe-instances --filters Name=instance.group-name,Values=midterm > awscli.txt
+	aws ec2 describe-security-groups --filters Name=group-name,Values=midterm >> awscli.txt
+}
+
+
+function create_backup() {
+		ls ./*.txt
+		for file in ./*.txt;
+do cp "$file" "$file".bak;
+done
+}
+
+
 if [ -z "$1" ]; then
 		system_update
 else
@@ -36,8 +51,14 @@ case "$ACTION" in
 	-m|--metadata)
 		save_metadata
 		;;
+	-a|--aws)
+		show_config
+		;;
+	-b|--backup)
+		create_backup
+		;;
 	*)
-	echo "Usage ${0} {-v|--version|-m|--metadata}"
+	echo "Usage ${0} {-b|--backup|-v|--version|-m|--metadata}"
 	exit 1
 esac
 fi
